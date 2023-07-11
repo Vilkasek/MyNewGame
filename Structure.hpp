546233451cffaa10e5b7666c0862586fac4e07bf
Jakub Brandt <tv.brandon.nr1@gmail.com>
2023-07-11 09:28:07 +0200

    Finished parent struct for structures

--- Structure.hpp
+++ Structure.hpp
@@ -6,5 +6,17 @@ struct Structure
     Texture2D tex = { 0 };
     Rectangle collBox = { 0 };
 
+    // Position
+    Vector2 pos = { 0 };
+
+    // Initialing function
+    void init();
+
+    // Rendering function
     void render();
+
+    void deinit()
+    {
+        UnloadTexture(tex);
+    }
 };
